1ac7589526a4a81ab7118b68fc33aaf4f1639399
이나경 <leena3823@gmail.com>
2024-07-21 01:41:53 +0900

    Update App.js

--- reactnavigation/App.js
+++ reactnavigation/App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ScreenA } from './src/ScreenA';
+import { ScreenB } from './src/ScreenB';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="ScreenA" component={ScreenA} />
+        <Stack.Screen name="ScreenB" component={ScreenB}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
